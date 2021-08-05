4d7248bff5383cb6126e7ffa6ccc4ea91de22542
Rayane Kettani <bafle31@gmail.com>
2021-08-05 17:04:21 +0200

    Reedition hors de open brush, du projet crypto-art-gallery

---
+++ Assets/Scripts/RandomTriggerEffect.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomTriggerEffect : MonoBehaviour
+{
+    public RandomTriggerSpawn randomTriggerSpawn;
+
+    // Update is called once per frame
+    void ChangeEffect(int effect)
+    {
+        switch (effect)
+        {
+            case 5:
+                Debug.Log("Why hello there good sir! Let me teach you about Trigonometry!");
+                break;
+            case 4:
+                Debug.Log("Hello and good day!");
+                break;
+            case 3:
+                Debug.Log("Whadya want?");
+                break;
+            case 2:
+                Debug.Log("Grog SMASH!");
+                break;
+            case 1:
+                Debug.Log("Ulg, glib, Pblblblblb");
+                break;
+            default:
+                Debug.Log("Incorrect intelligence level.");
+                break;
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "MainCamera")
+        {
+            randomTriggerSpawn.ChangePosition();
+            ChangeEffect(Random.Range(1, 6));
+        }
+    }
+}
---
+++ Assets/Scripts/RandomTriggerSpawn.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomTriggerSpawn : MonoBehaviour
+{
+    public GameObject TriggerPoint;
+    public GameObject MaxPoint;
+    public GameObject MinPoint;
+    public Vector3 NewRandomPos;
+    public float MaxRadius = 15;
+    public float MinRadius = 2;
+    public float Randomz = 3.1f;
+
+    private void Start()
+    {
+        ChangePosition();
+    }
+
+    public void ChangePosition()
+    {
+        Vector2 RandomPos = Random.insideUnitCircle * Random.Range(MinRadius, MaxRadius);
+        Vector3 newRandomPos = new Vector3(RandomPos.x, Randomz, RandomPos.y);
+        TriggerPoint.transform.position = newRandomPos * Random.Range(MinRadius, MaxRadius);
+        MaxPoint.transform.position = newRandomPos * MaxRadius;
+        MinPoint.transform.position = newRandomPos * MinRadius;
+        
+    }
+
+}
